6078f2c087cb9e2d6b13afbdb72bd6215566e086
Stephanie Haas <stephanie.haas@uni-graz.at>
2026-02-09 07:52:37 +0000

    compared Gr and AGR settings

--- src/teametrics/utils/ERA5/prep_ERA5-hourly.py
+++ src/teametrics/utils/ERA5/prep_ERA5-hourly.py
@@ -16,5 +16,18 @@ def run():
         ds.close()
         t2m.close()
 
+def compress_2023to2025_data():
+    files = sorted(glob.glob(
+        '/data/arsclisys/normal/clim-hydro/TEA-Indicators/ERA5/EUR/hourly/single_levels/prepped4TEA/*nc'))
+    files = files[-3:]
+
+    for file in files:
+        fname = file[:-3]
+        ds = xr.open_dataset(file)
+        ds = ds.rename({'valid_time': 'time'})
+        ds.to_netcdf(f'{fname}_NEW.nc',
+                     encoding={'T': {'dtype': 'float32', 'zlib': True, 'complevel': 5}})
+
 if __name__ == '__main__':
-    run()
+    # run()
+    compress_2023to2025_data()
