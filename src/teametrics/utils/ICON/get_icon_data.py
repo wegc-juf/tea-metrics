816d969acbc86deaba45c6070b37b7f508347b87
Jürgen Fuchsberger <juergen.fuchsberger@uni-graz.at>
2026-06-23 09:54:37 +0000

    get_icon_data.py: added icon-d2 data

--- src/teametrics/utils/ICON/get_icon_data.py
+++ src/teametrics/utils/ICON/get_icon_data.py
@@ -55,8 +55,58 @@ def download_icon_eu_t2m():
         print(f"downloaded + unpacked {fhr}")
 
 
+def download_icon_d2_t2m():
+    base = (
+        "https://opendata.dwd.de/weather/nwp/icon-d2/grib/"
+        f"{run}/t_2m/"
+    )
+    date = datetime.date.today().strftime("%Y%m%d")
+    
+    outdir = Path("icon_d2_t2m")
+    outdir.mkdir(exist_ok=True)
+
+    for fh in range(0, 49):
+
+        fhr = f"{fh:03d}"
+
+        bz2_name = (
+            f"icon-d2_germany_regular-lat-lon_single-level_"
+            f"{date}{run}_{fhr}_2d_t_2m.grib2.bz2"
+        )
+        
+
+        grib_name = bz2_name[:-4]  # remove .bz2
+
+        url = base + bz2_name
+
+        print(f"Downloading {url} ...")
+        r = requests.get(url, timeout=60)
+
+        if r.status_code != 200:
+            print(f"missing: {fhr}")
+            continue
+
+        bz2_path = outdir / bz2_name
+        grib_path = outdir / grib_name
+
+        # Save compressed file
+        with open(bz2_path, "wb") as f:
+            f.write(r.content)
+
+        # Decompress
+        with bz2.open(bz2_path, "rb") as fin:
+            with open(grib_path, "wb") as fout:
+                shutil.copyfileobj(fin, fout)
+
+        # Remove compressed archive
+        bz2_path.unlink()
+
+        print(f"downloaded + unpacked {fhr}")
+
+
 def run_main():
     download_icon_eu_t2m()
+    download_icon_d2_t2m()
     
 if __name__ == "__main__":
     run_main()
