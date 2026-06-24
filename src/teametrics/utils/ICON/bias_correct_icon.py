41b0a3c9ad246e6fb0467bc80ec119750bbc707e
Jürgen Fuchsberger <juergen.fuchsberger@uni-graz.at>
2026-06-24 07:42:17 +0000

    continued implementation of bias correction

--- src/teametrics/utils/ICON/bias_correct_icon.py
+++ src/teametrics/utils/ICON/bias_correct_icon.py
@@ -1,4 +1,7 @@
 import xarray as xr
+from pathlib import Path
+import numpy as np
+import matplotlib.pyplot as plt
 
 
 def extend_spartacus_with_icon_single(
@@ -98,28 +101,34 @@ def compute_bias_field_rolling(
     """
 
     common_days = np.intersect1d(
-        spartacus_tmax.time.values,
+        spartacus_tmax.time.values + hack_time_shift,
         icon_hist_tmax.time.values,
     )
 
-    obs = spartacus_tmax.sel(time=common_days)
+    obs = spartacus_tmax.sel(time=common_days - hack_time_shift)
+    # shift time of observations to match ICON forecast time (valid_time)
+    if hack_time_shift != 0:
+        obs = obs.assign_coords(time=common_days)
     fc = icon_hist_tmax.sel(time=common_days)
+    # convert K to °C
+    fc = fc - 273.15
 
     daily_error = obs - fc
     
+    window = min(window, common_days.size)
     weights = xr.DataArray(
-        [1, 2, 3, 4, 5, 6, 7],
+        [1, 2, 3, 4, 5, 6, 7][:window],
         dims=["time"]
     )
     
     bias = (
         daily_error
-        .isel(time=slice(-7, None))
+        .isel(time=slice(-window, None))
         .weighted(weights)
         .mean("time")
     )
 
-    return bias.isel(time=-1)
+    return bias
 
 
 def bias_correct_forecast(
@@ -135,29 +144,112 @@ def bias_correct_forecast(
 
 def forecast_daily_tmax(
     corrected_hourly: xr.DataArray,
+        var_name
 ):
     return (
         corrected_hourly
-        .groupby("valid_time.date")
+        .resample(time="1D")
         .max()
-        .rename("tmax")
+        .rename(var_name)
     )
+
+
+def first_day_tmax(ds, temp_var="t2m"):
+    """
+    Extract Tmax for the first forecast day of an ICON run.
+
+    Parameters
+    ----------
+    ds : xr.Dataset
+        ICON dataset with valid_time coordinate.
+
+    temp_var : str
+        Temperature variable name.
+
+    Returns
+    -------
+    xr.DataArray
+        Tmax field for forecast day 1.
+    """
+
+    first_time = ds.time.min().values
+    
+    day_start = first_time.astype("datetime64[D]")
+    day_end = day_start + np.timedelta64(1, "D")
+
+    return (
+        ds[temp_var]
+        .sel(time=slice(day_start, day_end))
+        .max("time")
+    )
+
+
+def first_day_tmax_stack(runs, temp_var="t2m"):
+    """
+    Extract first-day Tmax from a list of ICON runs.
+
+    Returns
+    -------
+    xr.DataArray
+
+    Dimensions:
+        run
+        latitude
+        longitude
+    """
+
+    tmax_list = []
+    
+    for run_file in runs:
+        ds = xr.open_dataset(run_file)
+
+        tmax = first_day_tmax(ds, temp_var)
+
+        run_time = ds.time.values[0]
+
+        tmax = tmax.expand_dims(
+            time=[run_time]
+        )
+
+        tmax_list.append(tmax)
+
+    return xr.concat(tmax_list, dim="time")
 
 
 def run_main():
-    bias = compute_bias_field_rolling( spartacus_tmax,
-        icon_hist_tmax,
-        window=7,
-    )
+    spartacus_data = "/data/reloclim/backup/ZAMG_SPARTACUS/data/current/SPARTACUS2-DAILY_TX_2026.nc"
+    spartacus_tmax = xr.open_dataarray(spartacus_data)
+    
+    for icon_dir in ["icon_eu_t2m_regridded", "icon_d2_t2m_regridded", "icon_t2m_regridded"]:
+        if not Path(icon_dir).exists():
+            print(f"Directory {icon_dir} does not exist. Please run regrid_icon_to_spcs.py first.")
+            continue
+        icon_hist_tmax = first_day_tmax_stack(
+            sorted(Path(icon_dir).glob("*.nc"))
+        )
+        bias = compute_bias_field_rolling(spartacus_tmax,
+            icon_hist_tmax,
+            window=7,
+        )
+        
+        todays_run = sorted(Path(icon_dir).glob("*.nc"))[-1]
+        icon_today = xr.open_dataset(todays_run)
+        icon_today = icon_today - 273.15  # convert K to °C
 
-    corrected_hourly = bias_correct_forecast(
-        icon_today["t2m"],
-        bias,
-    )
+        corrected_hourly = bias_correct_forecast(
+            icon_today["t2m"],
+            bias,
+        )
 
-    corrected_tmax = forecast_daily_tmax(
-        corrected_hourly
-    )
+        corrected_tmax = forecast_daily_tmax(
+            corrected_hourly[:-1],
+            spartacus_tmax.name
+        )
+        print(corrected_tmax)
+        filename = f"{icon_dir}_bias_corr/bias_corrected_tmax_{todays_run.stem}.nc"
+        if not Path(f"{icon_dir}_bias_corr").exists():
+            Path(f"{icon_dir}_bias_corr").mkdir(parents=True, exist_ok=True)
+        corrected_tmax.to_netcdf(filename)
 
 if __name__ == "__main__":
     run_main()
