537aa245c18a97f295834aa421d19bbf7b10dd50
Jürgen Fuchsberger <juergen.fuchsberger@uni-graz.at>
2026-07-29 08:39:00 +0000

    scripts: added `spartacus_workflow.sh` for SPARTACUS data regridding task

---
+++ internal/scripts/spartacus_workflow.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+# regrid SPCS data
+./src/teametrics/utils/SPARTACUS/regrid_SPARTACUS.py --config-file /home/juf/TEA-indicators/internal/config/WEGC/heatwave_paper_Tx30.yaml --year 2026
