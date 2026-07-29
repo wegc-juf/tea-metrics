74a3a3f01ac68b63ee1050b86049a0d3501b7a98
Jürgen Fuchsberger <juergen.fuchsberger@uni-graz.at>
2026-07-29 14:12:15 +0000

    scripts: add bias correction to ICON workflow and update SPARTACUS config paths

--- internal/scripts/spartacus_workflow.sh
+++ internal/scripts/spartacus_workflow.sh
@@ -4,18 +4,16 @@ cd ~/TEA-indicators/src
 # download and regrid ICON data
 ./teametrics/utils/ICON/get_icon_data.py
 ./teametrics/utils/ICON/regrid_icon_to_spcs.py
+./teametrics/utils/ICON/bias_correct_icon.py
 
 # regrid SPCS data
-./teametrics/utils/SPARTACUS/regrid_SPARTACUS.py --config-file /home/juf/TEA-indicators/internal/config/WEGC/heatwave_paper_Tx30.yaml --year 2026
+./teametrics/utils/SPARTACUS/regrid_SPARTACUS.py --config-file /home/juf/TEA-indicators/internal/config/WEGC/heatwave_paper_Tx30_update.yaml --year 2026
 
 # recalc daily and ctp data
-python -m teametrics.calc_TEA -
--config-file /home/juf/TEA-indicators/internal/config/WEGC/heatwave_paper_Tx30_update
-.yaml --loglevel INFO
+python -m teametrics.calc_TEA --config-file /home/juf/TEA-indicators/internal/config/WEGC/heatwave_paper_Tx30_update.yaml --loglevel INFO
 
 # detrend data
-~/wegenerNet/misc_analyses/climate_trends/detrend.py --folder-spartacus
-/data/arsclisys/normal/clim-hydro/TEA-Indicators/SPARTACUS/reproj_StatAT/v2.1  --end 2025 --data-var Tx --output-folder /data/arsclisys/normal/SPARTACUS/reproj_StatAT/detrended/ --year 2026 --cache
+# ~/wegenerNet/misc_analyses/climate_trends/detrend.py --folder-spartacus /data/arsclisys/normal/clim-hydro/TEA-Indicators/SPARTACUS/reproj_StatAT/v2.1  --end 2025 --data-var Tx --output-folder /data/arsclisys/normal/SPARTACUS/reproj_StatAT/detrended/ --year 2026 --cache
 
 # plot heatwave data
 ../plots/heatwave/plot_heatwave.py -cf ~/TEA-indicators/internal/config/WEGC/heatwave_paper_Tx30_update.yaml
