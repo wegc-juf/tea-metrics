76b754626a7e3d7a77058a4af1b9b81ff064fac2
Jürgen Fuchsberger <juergen.fuchsberger@uni-graz.at>
2026-07-29 09:26:06 +0000

    scripts: updated `spartacus_workflow.sh` to include TEA recalculation and directory change

--- internal/scripts/spartacus_workflow.sh
+++ internal/scripts/spartacus_workflow.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
 
+cd ~/TEA-indicators/src
 # regrid SPCS data
-./src/teametrics/utils/SPARTACUS/regrid_SPARTACUS.py --config-file /home/juf/TEA-indicators/internal/config/WEGC/heatwave_paper_Tx30.yaml --year 2026
+./teametrics/utils/SPARTACUS/regrid_SPARTACUS.py --config-file /home/juf/TEA-indicators/internal/config/WEGC/heatwave_paper_Tx30.yaml --year 2026
+
+# recalc daily and ctp data
+python -m teametrics.calc_TEA -
+-config-file /home/juf/TEA-indicators/internal/config/WEGC/heatwave_paper_Tx30_update
+.yaml --loglevel INFO
