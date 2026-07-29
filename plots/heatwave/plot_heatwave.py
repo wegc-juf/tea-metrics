f67f414bf946f1984485c10385621a3332f2609d
Jürgen Fuchsberger <juergen.fuchsberger@uni-graz.at>
2026-07-29 14:16:53 +0000

    plots/heatwave: adjust heatwave period end date for first event

--- plots/heatwave/plot_heatwave.py
+++ plots/heatwave/plot_heatwave.py
@@ -206,7 +206,7 @@ def run_main(opts, detrend_ctp="JJA"):
         configured_output_path / f"SPARTACUS_detrended_{detrend_ctp}" / "daily_basis_variables"
     ) + "/"
     
-    heatwave_period = ["2026-06-17", "2026-07-19"]
+    heatwave_period = ["2026-06-17", "2026-07-01"]
     worker(daily_data_path_detrended, daily_data_path_real_world, data_var, heatwave_period, detrend_ctp=detrend_ctp)
     
     heatwave_period = ["2026-07-25", "2026-08-06"]
