bc13738d384a128a028640eeab458ccb0e791d72
Jürgen Fuchsberger <juergen.fuchsberger@uni-graz.at>
2026-07-31 10:21:21 +0000

    scripts/spartacus_workflow.sh: activate venv, update PYTHONPATH, and add detrending, recalculation, and plotting steps

--- internal/scripts/spartacus_workflow.sh
+++ internal/scripts/spartacus_workflow.sh
@@ -1,5 +1,10 @@
 #!/usr/bin/env bash
 
+source /home/juf/TEA-indicators/.venv/bin/activate
+
+
+export PYTHONPATH=$PYTHONPATH:$HOME/wegenerNet/WPS/QCS:$HOME/wegenerNet/WPS/DPG:$HOME/wegenerNet/WPS:$HOME/wegenerNet:$HOME/wegenerNet/misc_analyses:~/cdr_DPS/scripts
+
 set -Eeuo pipefail
 
 LOG_DIR="$HOME/.local/state/tea-indicators"
@@ -31,7 +36,18 @@ run_step ./teametrics/utils/SPARTACUS/regrid_SPARTACUS.py --config-file /home/ju
 run_step python -m teametrics.calc_TEA --config-file /home/juf/TEA-indicators/internal/config/WEGC/heatwave_paper_Tx30_update.yaml --loglevel INFO
 
 # detrend data
-# ~/wegenerNet/misc_analyses/climate_trends/detrend.py --folder-spartacus /data/arsclisys/normal/clim-hydro/TEA-Indicators/SPARTACUS/reproj_StatAT/v2.1  --end 2025 --data-var Tx --output-folder /data/arsclisys/normal/SPARTACUS/reproj_StatAT/detrended/ --year 2026 --cache
+run_step ~/wegenerNet/misc_analyses/climate_trends/detrend.py --folder-spartacus /data/arsclisys/normal/clim-hydro/TEA-Indicators/SPARTACUS/reproj_StatAT/v2.1  --end 2025 --data-var Tx --output-folder /data/arsclisys/normal/SPARTACUS/reproj_StatAT/detrended/ --year 2026 --cache
+
+# recalc daily and ctp data for detrend
+run_step python -m teametrics.calc_TEA --config-file /home/juf/TEA-indicators/internal/config/WEGC/CW2_daily.yaml --loglevel INFO
 
 # plot heatwave data
 run_step ../plots/heatwave/plot_heatwave.py -cf "$HOME/TEA-indicators/internal/config/WEGC/heatwave_paper_Tx30_update.yaml"
+run_step rsync -av  /data/arsclisys/normal/clim-hydro/TEA-Indicators/results/heatwaves /mnt/unicloud/juergen.fuchsberger/TEA-indicators/heatwaves/202607
+
+# calculate decadal data
+run_step python -m teametrics.calc_TEA --config-file /home/juf/TEA-indicators/internal/config/WEGC/heatwave_paper_Tx30_decadal.yaml --loglevel INFO
+run_step python -m teametrics.calc_TEA --config-file /home/juf/TEA-indicators/internal/config/WEGC/heatwave_paper_Tx30_decadal_detrend.yaml --loglevel INFO
+
+# plot data
+run_step /home/juf/TEA-indicators/plots/paper-figs/plot_Fig5.py
