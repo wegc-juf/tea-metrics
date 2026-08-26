5a004110050c9c1e84a54550f1b7e4f626a955a8
Jürgen Fuchsberger <juergen.fuchsberger@uni-graz.at>
2026-08-26 15:16:24 +0000

    Add `--period` argument to heatwave plotting steps in `spartacus_workflow.sh`
    - Specify custom heatwave periods (2026-08-19 to 2026-09-15) for consistency across configurations.

--- internal/scripts/spartacus_workflow.sh
+++ internal/scripts/spartacus_workflow.sh
@@ -144,9 +144,9 @@ fi
 
 if stage_enabled --plot-heatwave; then
     # plot heatwave data
-    run_step ../plots/heatwave/plot_heatwave.py -cf "$HOME/TEA-indicators/internal/config/WEGC/heatwave_paper_Tx30_update.yaml"
-    run_step ../plots/heatwave/plot_heatwave.py -cf "$HOME/TEA-indicators/internal/config/WEGC/heatwave_paper_Tx35_update.yaml"
-    run_step ../plots/heatwave/plot_heatwave.py -cf "$HOME/TEA-indicators/internal/config/WEGC/heatwave_paper_Tx40_update.yaml"
+    run_step ../plots/heatwave/plot_heatwave.py -cf "$HOME/TEA-indicators/internal/config/WEGC/heatwave_paper_Tx30_update.yaml" --period 2026-08-19 2026-09-15
+    run_step ../plots/heatwave/plot_heatwave.py -cf "$HOME/TEA-indicators/internal/config/WEGC/heatwave_paper_Tx35_update.yaml" --period 2026-08-19 2026-09-15
+    run_step ../plots/heatwave/plot_heatwave.py -cf "$HOME/TEA-indicators/internal/config/WEGC/heatwave_paper_Tx40_update.yaml" --period 2026-08-19 2026-09-15
 fi
 
 if stage_enabled --sync; then
@@ -171,7 +171,7 @@ if stage_enabled --styria; then
     run_step python -m teametrics.calc_TEA --config-file /home/juf/TEA-indicators/internal/config/WEGC/CW2_daily_Styria.yaml --loglevel INFO
 
     # plot heatwave data
-    run_step ../plots/heatwave/plot_heatwave.py -cf "$HOME/TEA-indicators/internal/config/WEGC/Tx30_Styria_StatATGrid.yaml"
+    run_step ../plots/heatwave/plot_heatwave.py -cf "$HOME/TEA-indicators/internal/config/WEGC/Tx30_Styria_StatATGrid.yaml" --period 2026-08-19 2026-09-15
 
     # calculate decadal data
     run_step python -m teametrics.calc_TEA --config-file /home/juf/TEA-indicators/internal/config/WEGC/Tx30_Styria_decadal.yaml --loglevel INFO
